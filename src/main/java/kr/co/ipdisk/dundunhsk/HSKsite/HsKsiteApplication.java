9762d7731951d0096be81428e45d2fd67d91eaa0
HSK-DunDunRST <gon5073@naver.com>
2025-03-28 19:10:06 +0900

    프로젝트 생성(초기생성)

---
+++ src/main/java/kr/co/ipdisk/dundunhsk/HSKsite/HsKsiteApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ipdisk.dundunhsk.HSKsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsKsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsKsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/ipdisk/dundunhsk/HSKsite/HsKsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.ipdisk.dundunhsk.HSKsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsKsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
